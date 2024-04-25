8c79715282e6464612cedeb8562973baed6a4791
ChithraB009 <167099209+ChithraB009@users.noreply.github.com>
2024-04-25 10:21:51 +0530

    zigzaglevel.py

---
+++ zigzaglevel.py
@@ -0,0 +1,32 @@
+
+def printZigZagLevelOrder(root):
+    result = []
+    Q = [root]
+    level = 0 
+ 
+    while len(Q) > 0:
+        n = len(Q)
+        subResult = []
+        for i in range(n):
+            # step-1: popping
+            curr = Q.pop(0)
+            subResult.append(curr.data)
+ 
+            # step-2: pushing left child if its not none 
+            if curr.left != None:
+                Q.append(curr.left)
+ 
+            # step-3: pushing right child if its not none
+            if curr.right != None:
+                Q.append(curr.right)
+ 
+        if level % 2 == 1:
+            subResult = subResult[::-1]
+ 
+        result.append(subResult)
+        level += 1 
+ 
+    #print(result)
+    for subLevel in result:
+        print(*subLevel) 
+ 
