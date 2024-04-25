f8db56801c717876d60fed26433195f0fb77e844
ChithraB009 <167099209+ChithraB009@users.noreply.github.com>
2024-04-25 10:23:09 +0530

    leftView.py

---
+++ leftView.py
@@ -0,0 +1,23 @@
+def leftViewOfBinaryTree(root):
+    result = []
+    Q = [root]
+ 
+    while len(Q) > 0:
+        n = len(Q)
+        for i in range(n):
+            # step-1: popping
+            curr = Q.pop(0)
+            if i == 0:
+                result.append(curr.data)
+ 
+            # step-2: pushing left child if its not none 
+            if curr.left != None:
+                Q.append(curr.left)
+ 
+            # step-3: pushing right child if its not none
+            if curr.right != None:
+                Q.append(curr.right)
+ 
+    print("Left view is:")
+    print(result)
+ 
